0f618c0e6a0c6e32ef122985e52260a127fa7c97
deepthi <deepthi.varanasi@iths.se>
2021-04-07 18:42:31 +0200

    Final

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,9 +2,11 @@ package hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
     }
     public String testMethod(String name){
+
         return name;
     }
 }
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,5 +1,14 @@
+import hello.HelloWorld;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloWorldTest {
+ @Test
+    public void testMethod(){
+        HelloWorld helloWorld=new HelloWorld();
+        String result = helloWorld.testMethod("xxx");
+        assertEquals("xxx",result);
+    }
 
 }
